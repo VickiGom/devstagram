d6474bd6804430774521aafd5232b492cfbcf3f0
Victor Gomez <victor.gmz.lga28@gmail.com>
2022-09-14 14:44:16 -0600

    img

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -34,10 +34,7 @@
                         value=""
                         accept=".jpg, .jpeg, .png"
                         />
-                        @error('imagen')
-                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}
-                        </p>
-                        @enderror
+                    
                     </div>
                     <input 
                     type="submit"
